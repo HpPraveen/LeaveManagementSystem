ff92229b4abbb912e50e8acdec997f101e40a12c
Pasindu Praveen <pasindup@eteknowledge.com>
2021-03-05 15:51:52 +0530

    emp

---
+++ Controllers/EmployeeMastersController.cs
@@ -0,0 +1,54 @@
+﻿using LeaveManagementSystem.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace LeaveManagementSystem.Controllers
+{
+    public class EmployeeMastersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            return View(db.EmployeeMaster.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeId,EmployeeCode,EmployeeName,EmployeeSupervisorCode,EmployeeLeavePacakge")] EmployeeMaster employeeMaster)
+        {
+            if (employeeMaster.EmployeeCode != null && employeeMaster.EmployeeLeavePacakge != null && employeeMaster.EmployeeName != null && employeeMaster.EmployeeSupervisorCode != null &&
+                employeeMaster.EmployeeCode != "" && employeeMaster.EmployeeLeavePacakge != "" && employeeMaster.EmployeeName != "" && employeeMaster.EmployeeSupervisorCode != "")
+            {
+                var existingEmployee = db.EmployeeMaster.ToList().Where(e => e.EmployeeCode == employeeMaster.EmployeeCode).ToList();
+
+                if (existingEmployee.Count == 0)
+                {
+                    if (ModelState.IsValid)
+                    {
+                        db.EmployeeMaster.Add(employeeMaster);
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                    return View(employeeMaster);
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Employee Exist !!! ";
+                }
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Please fill all the fields !";
+            }
+            return View();
+        }
+    }
+}
--- Models/EmployeeMaster.cs
+++ Models/EmployeeMaster.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -11,9 +12,16 @@ namespace LeaveManagementSystem.Models
         [Key]
         public long EmployeeId { get; set; }
 
+        [DisplayName("Employee Code")]
         public string EmployeeCode { get; set; }
+
+        [DisplayName("Employee Name")]
         public string EmployeeName { get; set; }
+
+        [DisplayName("Supervisor Code")]
         public string EmployeeSupervisorCode { get; set; }
+
+        [DisplayName("Leave Package")]
         public string EmployeeLeavePacakge { get; set; }
     }
 }
