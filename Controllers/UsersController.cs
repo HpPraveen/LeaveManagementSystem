6935238d4827708b1947b515be11548cde362f97
Pasindu Praveen <pasindup@eteknowledge.com>
2021-03-06 00:08:13 +0530

    lo

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -17,7 +17,44 @@ namespace LeaveManagementSystem.Controllers
 
         public ActionResult Login(string username, string password)
         {
-            return RedirectToAction("Create", "LeaveRequest");
+            if (username != null && username != "" && password != null && password != "")
+            {
+                var userDetails = db.User.ToList().Where(e => e.Username == username && e.Password == password).ToList();
+                var empName = db.EmployeeMaster.ToList().Where(e => e.EmployeeCode == username).ToList().FirstOrDefault().EmployeeName;
+
+                if (userDetails.Count > 0)
+                {
+                    if (userDetails.FirstOrDefault().UserId != 0)
+                    {
+                        var userRole = db.UserRole.ToList().Where(e => e.UserId == userDetails.FirstOrDefault().UserId).FirstOrDefault().Role;
+                        if (userRole == "Employee")
+                        {
+                            TempData["LoggedEmployeeCode"] = username;
+                            TempData["LoggedEmployee"] = empName;
+                            return RedirectToAction("Create", "LeaveRequest");
+                        }
+                        else if (userRole == "Supervisor")
+                        {
+                            TempData["LoggedSupervisorCode"] = username;
+                            TempData["LoggedSupervisorName"] = empName;
+                            return RedirectToAction("Create", "LeaveRequest");
+                        }
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = "Please check your login credentials !";
+                    }
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Please check your login credentials !";
+                }
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Please fill all the fields !";
+            }
+            return Redirect(this.Request.UrlReferrer.AbsolutePath);
         }
     }
 }
